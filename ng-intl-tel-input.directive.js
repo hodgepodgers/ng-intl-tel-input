78aa92235ad0cff206a319b854030d1bca38a164
bighappyface <anomalous20@gmail.com>
2015-08-05 10:46:00 -0500

    feat(index.html): add functional example page

---
+++ ng-intl-tel-input.directive.js
@@ -0,0 +1,12 @@
+angular.module('ngIntlTelInput', []);
+angular.module('ngIntlTelInput')
+  .directive('ngIntlTelInput', function () {
+    return{
+      restrict: 'A',
+      require: "ngModel",
+      link: function (scope, elm, attr, ctrl) {
+        if (attr.type !== 'text') return;
+        elm.intlTelInput();
+      }
+    };
+  });
