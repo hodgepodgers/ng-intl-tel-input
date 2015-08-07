6bfcb17a4a237e25b0056e4c3404a74cb5d4721e
John Lozano <john.lozano@rackspace.com>
2015-08-06 23:18:34 -0500

    feat(directive): Default Country Attribute

--- ng-intl-tel-input.directive.e2e.js
+++ ng-intl-tel-input.directive.e2e.js
@@ -6,13 +6,26 @@ var page = Object.create({}, {
     set: function (number) {
       this.phone.sendKeys(number);
     }
+  },
+  phoneDefaultCountry: {
+    get: function () {
+      return $('input#tel-dc');
+    },
+    set: function (number) {
+      this.phoneDefaultCountry.sendKeys(number);
+    }
   }
 });
 
 describe('ng-intl-tel-input directive', function () {
   it('should properly format a phone number', function () {
-    page.phone = '18002255288';
+    page.phone= '18002255288';
     expect(page.phone.getAttribute('value')).to.eventually.equal('1 800-225-5288');
+  });
+
+  it('should set default country to data-default-country', function () {
+    page.phoneDefaultCountry = '07400123456';
+    expect(page.phoneDefaultCountry.getAttribute('value')).to.eventually.equal('07400 123456');
   });
 });
 
