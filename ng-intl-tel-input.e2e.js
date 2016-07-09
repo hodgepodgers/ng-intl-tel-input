c232a28f2d89e100eb535c76a71583036ff53a8f
bighappyface <anomalous20@gmail.com>
2016-07-09 13:47:56 -0500

    feat(directive): add selected country data setting

--- ng-intl-tel-input.e2e.js
+++ ng-intl-tel-input.e2e.js
@@ -27,4 +27,12 @@ describe('ng-intl-tel-input directive', function () {
     page.phoneDefaultCountry = '07400123456';
     expect(page.phoneDefaultCountry.getAttribute('value')).to.eventually.equal('07400123456');
   });
+
+  it('should set the selected country data when a different country is selected', function () {
+    element.all(by.css('.selected-flag')).get(1).click();
+    element.all(by.css('.flag-container .country[data-country-code="af"] .flag-box .iti-flag')).get(1).click();
+    page.phoneDefaultCountry = '1';
+    expect(element(by.id('selectedCountry')).getText()).to.eventually.contain('Afghanistan');
+  });
+
 });
