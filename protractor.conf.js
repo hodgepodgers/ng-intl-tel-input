254eb9679a29052ef2ec866ef942aee5ed755496
SyntaxStacks <johnbobsexipants@gmail.com>
2018-02-11 20:36:01 -0600

    protractor updates

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 exports.config = {
   rootElement: 'html',
-  seleniumServerJar: __dirname + '/node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
+  seleniumServerJar: __dirname + '/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
 
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
